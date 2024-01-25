1a154ef4ca78b2b06353cbe90ad6b8fb4056d766
Piyush <sainipiyush8860@gmail.com>
2024-01-25 15:17:18 +0530

    commented vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 // https://vitejs.dev/config/
-const env = loadEnv(mode, process.cwd(), '')
 export default defineConfig({
   plugins: [react()]
   ,
   define: {
-    'process.env': {API :process.env.VITE_URL},
-    VITE_URL:JSON.stringify(env.VITE_URL)
+    'process.env': {API :process.env.VITE_URL}
+    // VITE_URL:JSON.stringify(env.VITE_URL)
   },
   resolve: {
     alias: {
