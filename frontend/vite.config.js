87158c87f4f98d62f2d8153ffe2d548d2a0a8bdb
Piyush <sainipiyush8860@gmail.com>
2024-01-25 14:26:56 +0530

    added env var in config.2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 // https://vitejs.dev/config/
+const env = loadEnv(mode, process.cwd(), '')
 export default defineConfig({
   plugins: [react()]
   ,
